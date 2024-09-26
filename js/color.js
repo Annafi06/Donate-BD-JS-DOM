79c9a1a4b56810e2b7e43845b94f0573c91f351e
Annafi06 <abdunnafi28@gmail.com>
2024-09-27 01:50:34 +0600

    done with the main page

--- js/color.js
+++ js/color.js
@@ -6,6 +6,9 @@ tailwind.config = {
                 'button-green': 'rgb(180, 244, 97)',
                 'gray-black': 'rgba(17, 17, 17, 0.7)',
                 'beig': 'rgb(249, 247, 243)',
+                'ash-black': 'rgba(17, 17, 17, 0.3)',
+                'border-black': 'rgba(17, 17, 17, 0.1)',
+                'halka': 'rgba(17, 17, 17, 0.05)',
             }
         }
     }
