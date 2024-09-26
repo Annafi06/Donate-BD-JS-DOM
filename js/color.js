a00045a04562e8249a22129b31912499fe84bfce
Annafi06 <abdunnafi28@gmail.com>
2024-09-26 22:24:21 +0600

    navbar initially done

---
+++ js/color.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                'button-green': 'rgb(180, 244, 97)',
+                'gray-black': 'rgba(17, 17, 17, 0.7)',
+                'beig': 'rgb(249, 247, 243)',
+            }
+        }
+    }
+}
